4a4367eedf4b48342b5719f7855235c9056bc29c
Ross Sbriscia <ross.sbriscia@optum.com>
2018-05-30 10:54:57 -0600

    Update handler.lua

--- src/handler.lua
+++ src/handler.lua
@@ -13,7 +13,6 @@ function KongUpstreamJWTHandler:access(conf)
   access.execute(conf)
 end
 
--- Set a low priority, because 'AUTH' plugins run with priority 1000, and we don't want to strip the Authorization header before the request reaches OAuth
 KongUpstreamJWTHandler.PRIORITY = 2500
 KongUpstreamJWTHandler.VERSION = "0.1.0"
 
