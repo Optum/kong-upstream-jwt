b2bafb2ecfe27ab480cae68b962642bfda7e35d4
Govender, Rowin <RGovender1@fnb.co.za>
2019-07-13 22:07:58 +0200

    Adding key identifier config and logic to allow setting of JWT header kid claim.

--- src/schema.lua
+++ src/schema.lua
@@ -4,6 +4,7 @@ return {
   fields = {
     issuer = { type = "string", required = false },
     private_key_location = { type = "string", required = false },
-    public_key_location = { type = "string", required = false }
+    public_key_location = { type = "string", required = false },
+    key_id = { type = "string", required = false}
   }
 }
