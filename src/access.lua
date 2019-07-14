c9baa81e49e83a1d6d91c8a038bcb0027c007476
Govender, Rowin <RGovender1@fnb.co.za>
2019-07-14 07:41:51 +0200

    Adding logic to allow user to set the name of the header key storing the JWT token, as well as a flag to include the credential type if set.

--- src/access.lua
+++ src/access.lua
@@ -140,14 +140,22 @@ local function build_payload_hash()
   return str.to_hex(payload_digest)
 end
 
---- Add the JWT header to the reqeust
+local function build_header_value(conf, jwt)
+  if (conf.include_credential_type == true) then
+    return "Bearer " .. jwt
+  else
+    return jwt
+  end
+end
+
+--- Add the JWT header to the request
 -- @param conf the configuration
 local function add_jwt_header(conf)
   local payload_hash = build_payload_hash()
   local payload = build_jwt_payload(conf, payload_hash)
   local kong_private_key = get_kong_key("pkey", get_private_key_location(conf))
   local jwt = encode_jwt_token(conf, payload, kong_private_key)
-  ngx.req.set_header("JWT", jwt)
+  ngx.req.set_header(conf.header, build_header_value(conf, jwt))
 end
 
 --- Execute the script
